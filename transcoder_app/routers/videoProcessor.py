afe64c06d80489ff5eabb4dc33df3c344e249115
adib-the-noob <mdadib550@gmai.com>
2024-09-04 22:45:41 +0600

    aws particular role added for app

--- transcoder_app/routers/videoProcessor.py
+++ transcoder_app/routers/videoProcessor.py
@@ -6,9 +6,19 @@ from schemas.videoModel import VideoModel
 from utils.video_utils import transcode_video
 from utils.aws_utils import upload_folder_to_s3
 
+from dotenv import load_dotenv
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
 router = APIRouter()
 
-s3_client = boto3.client('s3')
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+)
 
 @router.post('/download-s3-file')
 def download_s3_file(
--- transcoder_app/utils/aws_utils.py
+++ transcoder_app/utils/aws_utils.py
@@ -1,7 +1,17 @@
 import boto3
+import os
 
+from dotenv import load_dotenv
+load_dotenv()
 
-s3_client = boto3.client('s3')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+)
 
 import os
 import boto3
